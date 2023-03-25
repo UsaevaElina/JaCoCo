268459d556fcdba823728a2ad7f3e9756c942204
ElinaUsaeva <Usaeva1@yandex.ru>
2023-03-25 22:14:41 +0300

    2 Tests

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -2,11 +2,11 @@ package ru.netology.statistic;
 
 public class StatisticsService {
 
-    public long findMax(long[] incomes) {
-        long currentMax = incomes[0];
-        for (long income : incomes) {
-            if (currentMax < income) {
-                currentMax = income;
+    public long findMax(long[] incomes) { //12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12
+        long currentMax = incomes[0]; // в переменную currentMax скопировать значение 0 ячейки массива
+        for (long income : incomes) { //для каждого income(элемента массива) из массива incomes
+            if (currentMax < income) {//если элемент массива меньше income
+                currentMax = income;//блпблп
             }
         }
         return currentMax;
--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,16 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMin() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {0, 5, 8, 4, 5, 3, 8, 6, 11, 11, 600};
+        long expected = 600;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
